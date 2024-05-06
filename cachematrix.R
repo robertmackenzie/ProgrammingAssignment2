544b9edce9fef7cfb8b7a85862cf71b011f0acaa
Robert Mackenzie <robertmackenzie@gmail.com>
2024-05-06 07:07:03 +0100

    Implement CacheMatrix
    CacheMatrix provides a matrix implementation with a memoised invert operation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R provides a matrix implementation with a memoised invert operation
 
-## Write a short comment describing this function
+## Construct a "CacheMatrix" object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i
+    list(get = get, set = set,
+        getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## Invert a "CacheMatrix" object, memoising the result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if (!is.null(i)) {
+      print("using cached result")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
